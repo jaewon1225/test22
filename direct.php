576ca1002a0e46c0fe6226602fc5db9444adc1d5
jaewon1225 <58664211+jaewon1225@users.noreply.github.com>
2020-06-14 22:48:00 +0900

    Add files via upload

--- direct.php
+++ direct.php
@@ -106,27 +106,9 @@ for (var i = 0; i < positions1.length; i ++) {
         map: map, // 마커를 표시할 지도
         position: new kakao.maps.LatLng(positions1[i], positions2[i]),// 마커를 표시할 위치
         image : markerImage // 마커 이미지 
-        clickable: true
     });
 
-    // 마커를 지도에 표시합니다.
-marker.setMap(map);
 
-// 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-    iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-// 인포윈도우를 생성합니다
-var infowindow = new kakao.maps.InfoWindow({
-    content : iwContent,
-    removable : iwRemoveable
-});
-
-// 마커에 클릭이벤트를 등록합니다
-kakao.maps.event.addListener(marker, 'click', function() {
-      // 마커 위에 인포윈도우를 표시합니다
-      infowindow.open(map, marker);  
-});
 
 
     
